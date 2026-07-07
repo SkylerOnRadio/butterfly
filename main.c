21488eb47fdbaf851d1a7b1e55ca157f5ec858ce
SkylerOnRadio <abhigyadulal@gmail.com>
2026-07-07 17:20:50 +0530

    Added tokenising the line read

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *read_line() {
   char *line = NULL;
@@ -20,17 +21,50 @@ char *read_line() {
   return line;
 }
 
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\a\n\a"
+char **split_line(char *line) {
+  int bufsize = TOK_BUFSIZE;
+  char **tokens = malloc(bufsize * sizeof(char *));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "butterfly: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  int position = 0;
+  token = strtok(line, TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    ++position;
+
+    if (position >= bufsize) {
+      bufsize += TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize);
+      if (!tokens) {
+        fprintf(stderr, "butterfly: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, TOK_DELIM);
+  }
+
+  tokens[position] = NULL;
+  return tokens;
+}
+
 int loop() {
   int exit = 0;
 
   char *line;
+  char **tokens;
 
   while (exit != -1) {
     printf(">");
     line = read_line();
-    if (line == "exit")
-      exit = -1;
-    printf("%s\n", line);
+    tokens = split_line(line);
   }
 
   return exit;
